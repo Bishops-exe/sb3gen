Bishops-exe/sb3gen

commit d0c56c2bc17af570de9e7e885074731ca9a3f24b

refactor: migrate build to rollup, restructure block modules, drop lodash
Switch from bun build to rollup for ESM + minified + types outputs. Move
blocks/ to block/ (singular), add Control.ts, remove ScalarBuilder.ts,
update monitor to accept Sprite objects directly, update all examples.

src/project/asset/Costume.ts
@@ -1,68 +1,70 @@
-import 'reflect-metadata';
-import { Expose } from 'class-transformer';
-import { IsNumber, IsString } from 'class-validator';
+import {Expose} from 'class-transformer';
+import {IsNumber, IsString} from 'class-validator';
 import SparkMD5 from 'spark-md5';
 
 export default class Costume {
   @Expose()
   @IsString()
-  assetId: string;
-
-  @Expose()
-  @IsString()
   name: string;
 
-  @Expose()
-  @IsString()
-  md5ext: string;
+  data: Uint8Array;
+
 
   @Expose()
-  @IsString()
-  dataFormat: string;
+  get assetId(): string {
+    return SparkMD5.ArrayBuffer.hash(this.data.buffer as ArrayBuffer);
+  }
 
   @Expose()
-  @IsNumber()
-  bitmapResolution: number;
+  get md5ext(): string {
+    return `${this.assetId}.${this.dataFormat}`
+  }
 
   @Expose()
-  @IsNumber()
-  rotationCenterX: number;
+  get dataFormat(): string {
+    return Costume.detectFormat(this.data);
+  }
 
   @Expose()
-  @IsNumber()
-  rotationCenterY: number;
+  get bitmapResolution(): number {
+    return this.dataFormat === 'svg' ? 1 : 2;
+  }
 
-  data?: Uint8Array;
-
-  constructor(name: string, data?: Uint8Array) {
+  constructor(name: string, data: Uint8Array | string) {
     this.name = name;
-    if (!data) return;
-    this.data = data;
-    this.assetId = SparkMD5.ArrayBuffer.hash(data.buffer as ArrayBuffer);
-    this.dataFormat = Costume.detectFormat(data);
-    this.bitmapResolution = this.dataFormat === 'svg' ? 1 : 2;
-    this.md5ext = `${this.assetId}.${this.dataFormat}`;
+    this.data = (typeof data === "string" ? new TextEncoder().encode(data) : data)
   }
 
   static blank(name: string, width = 480, height = 360): Costume {
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"><rect width="${width}" height="${height}" fill="white"/></svg>`;
-    return new Costume(name, new TextEncoder().encode(svg));
+    return new Costume(name, `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}">
+        <rect width="${width}" height="${height}" fill="white"/>
+      </svg>
+    `);
   }
 
   static colored(name: string, color: string, width = 480, height = 360): Costume {
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"><rect width="${width}" height="${height}" fill="${color}"/></svg>`;
-    return new Costume(name, new TextEncoder().encode(svg));
+    return new Costume(name, `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}">
+        <rect width="${width}" height="${height}" fill="${color}"/>
+      </svg>
+    `);
   }
 
-  static circle(name: string, color: string, size = 80): Costume {
-    const r = size / 2;
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${size} ${size}"><circle cx="${r}" cy="${r}" r="${r - 2}" fill="${color}"/></svg>`;
-    return new Costume(name, new TextEncoder().encode(svg));
+  static circle(name: string, color: string, radius = 40): Costume {
+    return new Costume(name, `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${radius} ${radius}">
+        <circle cx="${radius}" cy="${radius}" r="${radius - 2}" fill="${color}"/>
+       </svg>
+    `);
   }
 
   static rect(name: string, color: string, width = 80, height = 80, rx = 0): Costume {
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"><rect width="${width}" height="${height}"${rx ? ` rx="${rx}"` : ''} fill="${color}"/></svg>`;
-    return new Costume(name, new TextEncoder().encode(svg));
+    return new Costume(name, `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}">
+        <rect width="${width}" height="${height}"${rx ? ` rx="${rx}"` : ''} fill="${color}"/>
+       </svg>
+    `);
   }
 
   private static detectFormat(data: Uint8Array): string {

examples/01-hello-world.js
@@ -11,7 +11,7 @@ const sprite = project.addSprite('Cat');
 sprite.costumes.push(Costume.circle('costume1', '#4C97FF', 100));
 
 sprite.addScript(s => {
-  s.push(WhenFlagClicked);
+  s.push(WhenFlagClicked());
   s.push(SayForSecs('Hello, World!', 2));
 });
 

examples/02-bouncing-ball.js
@@ -4,6 +4,7 @@
 import {
   Project, Costume,
   WhenFlagClicked, MoveSteps, IfOnEdgeBounce,
+  Forever,
 } from '../src';
 
 const project = new Project();
@@ -15,11 +16,11 @@ sprite.costumes.push(Costume.circle('ball', '#FF6680', 60));
 sprite.direction = 45;
 
 sprite.addScript(s => {
-  s.push(WhenFlagClicked);
-  s.forever(inner => {
+  s.push(WhenFlagClicked());
+  s.push(Forever(inner => {
     inner.push(MoveSteps(10));
-    inner.push(IfOnEdgeBounce);
-  });
+    inner.push(IfOnEdgeBounce());
+  }));
 });
 
 await project.save(`${import.meta.dir}/bouncing-ball.sb3`);

examples/04-broadcast.js
@@ -19,7 +19,7 @@ const sender = project.addSprite('Sender');
 sender.costumes.push(Costume.circle('costume1', '#5CB1D6', 80));
 sender.x = -80;
 sender.addScript(s => {
-  s.push(WhenThisSpriteClicked);
+  s.push(WhenThisSpriteClicked());
   s.push(BroadcastAndWait(greet));
 });
 

examples/05-edit-existing.js
@@ -43,8 +43,8 @@ if (sprite) {
 
 if (sprite) {
   sprite.addScript(s => {
-    s.xy(600, 0);
-    s.push(WhenFlagClicked);
+    s.setXY(600, 0);
+    s.push(WhenFlagClicked());
     s.push(Say('I was injected!'));
   });
 }

examples/06-clones.js
@@ -12,6 +12,7 @@ import {
   WhenFlagClicked, Wait,
   CreateCloneOf,
   WhenIStartAsAClone, GoToXY, Random, DeleteThisClone,
+  Forever,
 } from '../src';
 
 const project = new Project();
@@ -23,23 +24,23 @@ sprite.costumes.push(Costume.circle('particle', '#FFD700', 20));
 
 // Script 1: forever spawn a clone every 0.5 s
 sprite.addScript(s => {
-  s.push(WhenFlagClicked);
-  s.forever(inner => {
+  s.push(WhenFlagClicked());
+  s.push(Forever(inner => {
     inner.push(CreateCloneOf('_myself_'));
     inner.push(Wait(0.5));
-  });
+  }));
 });
 
-// Script 2: clone behaviour
+// Script 2: clone behavior
 sprite.addScript(s => {
-  s.xy(300, 0);
-  s.push(WhenIStartAsAClone);
+  s.setXY(300, 0);
+  s.push(WhenIStartAsAClone());
   s.push(GoToXY(
     s.embed(Random(-200, 200)),
     s.embed(Random(-150, 150))
   ));
   s.push(Wait(1.5));
-  s.push(DeleteThisClone);
+  s.push(DeleteThisClone());
 });
 
 await project.save(`${import.meta.dir}/clones.sb3`);

examples/07-operators.js
@@ -23,7 +23,7 @@ const sprite = project.addSprite('Math');
 sprite.costumes.push(Costume.circle('costume1', '#5CB1D6', 80));
 
 sprite.addScript(s => {
-  s.push(WhenFlagClicked);
+  s.push(WhenFlagClicked());
   s.push(SetVariableTo(s.embed(Random(1, 10)), pick));
   s.push(SetVariableTo(s.embed(Multiply(s.embed(pick), 2)), doubled));
   s.push(SayForSecs(s.embed(Join('pick \xd7 2 = ', s.embed(doubled))), 2));

examples/08-if-else.js
@@ -11,6 +11,7 @@ import {
   Project, Costume,
   WhenFlagClicked, SetVariableTo, WaitUntil, SayForSecs,
   Random, Gt, KeyPressed,
+  IfElse,
 } from '../src';
 
 const project = new Project();
@@ -23,14 +24,14 @@ const sprite = project.addSprite('Judge');
 sprite.costumes.push(Costume.rect('costume1', '#FFAB19', 80, 80, 12));
 
 sprite.addScript(s => {
-  s.push(WhenFlagClicked);
+  s.push(WhenFlagClicked());
   s.push(SetVariableTo(s.embed(Random(1, 10)), n));
 
-  s.ifElse(
+  s.push(IfElse(
     s.embed(Gt(s.embed(n), 5)),
     then => { then.push(SayForSecs('big!', 1)); },
     els  => { els.push(SayForSecs('small!', 1)); }
-  );
+  ));
 
   s.push(WaitUntil(s.embed(KeyPressed('space'))));
 });

commit 6a63974f3d207e1b9d8db63138f236ff22895e31

fix(costume): regression in circle only showing top left quarter
Fixes: d0c56c2b

src/project/asset/Costume.ts
@@ -52,8 +52,10 @@ export default class Costume {
   }
 
   static circle(name: string, color: string, radius = 40): Costume {
+    const diameter = radius * 2;
+
     return new Costume(name, `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${radius} ${radius}">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${diameter} ${diameter}">
         <circle cx="${radius}" cy="${radius}" r="${radius - 2}" fill="${color}"/>
        </svg>
     `);